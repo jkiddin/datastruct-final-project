16b8d7c1ed75c4f8da93ca46345fa9f0c3b6d834
josh <55262920+jkiddin@users.noreply.github.com>
2022-12-01 22:29:54 -0500

    added instructions to program for easy access

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "First commit." << std::endl;
+/*
+You need to implement insertion sort, selection sort, bubble sort, merge sort, quicksort, heapsort (increasing order) algorithms and measure the performance of these algorithms in terms of number of steps and CPU running time.  
+Task I: 
+Implement insertion sort, selection sort, bubble sort, merge sort, quicksort and heapsort to sort the numbers in the increasing order. Write the program to evaluate the performance of your algorithms in two ways: (1) CPU time; (2) total no. of steps.
+Input (not enter by the user) include:  
+1)	the number of integers to be sorted, n.
+2)	generating three instances as follows:
+a.	sorted input data set: 1, 2, 3, … n;
+b.	reversely sorted input data set: n, n-1, …, 3, 2,1;
+c.	random permutation of 1, 2, …, n (random permutation means that each number of 1, 2, …, n appears exactly once in the input data set but with random position. For example, if n=5, then you may have the random permutation 4, 1, 3, 5, 2).
+
+Test your program and do the experiments as follows:
+Run the above three generated input data sets for each n (number of integers) = 100, 300, 500, 1000, 3000, 5000, 10000, 50000
+Note: 
+(1) You may have to repeat the algorithm many times, each time you need to initialize the array. 
+(2) Your running time for each sorting algorithm should exclude the time for initialization.  
+(3) All measurement should be done in a single run, i.e. you do not need to run once for n=100, another time for n=200, etc
+What to turn in:
+(1) Well documented source code in C++
+(2) Report the number of steps and the CPU running time in a table 
+(3) Plot the running time of the algorithm results
+
+Task II: 
+For each algorithm, and for each n (number of integers) = 100, 300, 500, 1000, 3000, 5000, 10000, 50000 measure its average running time and average number of steps for 50 randomly generated instances. 
+Note the input data should include 50 sets of n numbers with each number generated randomly in the range of [1, n]. Accumulated the total CPU time and the total number of steps for all 50 data sets, and then get the average CPU time and average total number of steps by dividing the total value by 50.  
+What to turn in:
+(1) Well documented source code in C++
+(2) Report the number of steps and the CPU running time in a table 
+(3) Plot the running time of the algorithm results
+*/
+
+
     return 0;
 }
